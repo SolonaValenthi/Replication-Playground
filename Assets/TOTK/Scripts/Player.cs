1628cf6dfe30440f0ec4265c4f6826c174164080
Nikolai Nelson <nikolainelson@comcast.net>
2024-01-13 04:59:45 -0800

    initial player setup

---
+++ Assets/TOTK/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CinemachineFreeLook _playerCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
